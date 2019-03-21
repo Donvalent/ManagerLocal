dfed64430cc7f79560b258662afb82c78d26b94d
Valentin <Donvalent@yandex.ru>
2019-03-21 22:06:36 +0300

    add table in main page

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,11 +1,17 @@
 <?php
 
-    //include_once ROOT. '/models/....php';
+    include_once ROOT. '/models/Departments.php';
+    include_once ROOT. '/models/Employees.php';
 
     class SiteController
     {
         public function actionIndex()
         {
+            $departmentsList = array();
+            $departmentsList = Departments::getDepartmentsList();
+
+            $employeesList = array();
+            $employeesList = Employees::getEmployeesList();
                         
 
             require_once(ROOT . '/view/index.php');
--- view/index.php
+++ view/index.php
@@ -2,6 +2,30 @@
 <?php  include_once(ROOT . '/view/layouts/header.html'); ?>
 
 <!-- Body -->
-
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">Сотрудники</th>
+            <?php foreach($departmentsList as $departments => $department): ?>
+                <th scope="col"><?php echo $department['title']; ?></th>
+            <?php endforeach; ?>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($employeesList as $employees => $employee): ?>
+            <tr>
+                <td><?php echo $employee['surname'];?></td>
+                <?php foreach($departmentsList as $departments => $department): ?>
+                    <?php
+                        if(in_array($department['title'], $employee['departments']))
+                            echo '<td>+</td>';
+                        else
+                            echo '<td> </td>';
+                    ?>
+                <?php endforeach; ?>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
 <!-- Footer -->
 <?php  include_once(ROOT . '/view/layouts/footer.html'); ?>
